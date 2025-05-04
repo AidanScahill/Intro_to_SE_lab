484de5de4f6a32e165d5bfa2bbe5f2e9e6732ef3
AidanScahill <aidancontrolled@gmail.com>
2025-05-04 02:20:52 -0500

    seller panel added

--- storefront/urls.py
+++ storefront/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('product_filter/<slug:category_slug>/', views.product_filter, name='product_filter'),
     path('product_detail/<slug:product_slug>/', views.product_detail, name='product_detail'),
     path('product_search/', views.product_search, name='product_search'),
+    path('seller_panel/', views.seller_panel, name='seller_panel'),
+    path('product_edit/<slug:product_slug>/', views.product_edit, name='product_edit'),
+    path('product_add/', views.product_add, name='product_add'),
+    
 
 ]
